4046c74aa8c03d5cc1d74183c7ef44bfcdaddd39
Milena-elise <74831701+Milena-elise@users.noreply.github.com>
2021-12-13 11:20:59 -0500

    Update Curve2.h

--- Curve2.h
+++ Curve2.h
@@ -1,1 +1,21 @@
 //Curve2.h
+#include "ScoreBank.h"
+
+
+class Curve2 : public ScoreBank 
+{
+    //size is still same, we wont be changing
+    protected:
+    int* scores1 = scores;
+    float avg1 = avg;
+    
+    public:
+    //constructor/deconstructor
+    Curve2();
+    ~Curve2();
+    
+    //methods
+    void Curve();
+    void printScores();
+
+};
